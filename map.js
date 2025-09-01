e57437c36354351be872a07f5396c3ab8cf7c376
Archeoisio <m.musio@yahoo.it>
2025-09-02 01:25:11 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   const MOBILE_MAX_WIDTH = 767;
-  const mobileView  = { center: [40, 10], zoom: 5 };
+  const mobileView  = { center: [45, 10], zoom: 5 };
   const desktopView = { center: [49, 30], zoom: 5 };
   const isMobile    = window.innerWidth <= MOBILE_MAX_WIDTH;
   const initialView = isMobile ? mobileView : desktopView;
