d4b497a5666fcb1ca332ea144eb98a8f3197c6a4
Archeoisio <m.musio@yahoo.it>
2025-09-01 17:39:14 +0200

    Update map.js

--- map.js
+++ map.js
@@ -233,8 +233,8 @@ document.addEventListener('DOMContentLoaded', () => {
   function updateLabels() {
     const zoom = map.getZoom();
     const minZoom = 5, maxZoom = 12;
-    const minFont = 6, maxFont = 18;
-    const minPadding = 2, maxPadding = 8;
+    const minFont = 8, maxFont = 18;
+    const minPadding = 4, maxPadding = 8;
 
     const factor = Math.min(Math.max((zoom - minZoom) / (maxZoom - minZoom), 0), 1);
 
