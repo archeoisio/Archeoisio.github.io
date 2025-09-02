e50413f432ed112f96fd61c6e17ea8ed61955e79
Archeoisio <m.musio@yahoo.it>
2025-09-02 20:53:57 +0200

    Update map.js

--- map.js
+++ map.js
@@ -53,7 +53,7 @@ window.addEventListener('orientationchange', resizeMap);
 resizeMap();
 
 // --- Funzione per precaricare le tiles tra zoom 5 e 14 ---
-function preloadTiles(map, minZ = 4, maxZ = 14) {
+function preloadTiles(map, minZ = 10, maxZ = 14) {
   const bounds = map.getBounds();
 
   for (let z = minZ; z <= maxZ; z++) {
