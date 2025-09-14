d5ab13a6d6058d47bea65a14dd45a2f8265b9d6e
Archeoisio <m.musio@yahoo.it>
2025-09-14 16:48:56 +0200

    Update map-flag.js

--- map-flag.js
+++ map-flag.js
@@ -282,14 +282,14 @@ const searchControl = L.Control.geocoder({
 
       // Aggiorna contenuto con FlyTo e mostra pannello
       content.innerHTML = `
-          <div style="font-size:16px;font-weight:bold; display:flex; justify-content:space-between; align-items:center;">
+          <div style="font-size:14px;font-weight:bold; display:flex; justify-content:space-between; align-items:center;">
           ${nation} 
           </div>
-        <div style="font-size:14px;font-weight:bold; color:white;">
+        <div style="font-size:12px;font-weight:bold; color:white;">
     ${name}
-    <button id="fly-btn" style="background:none;border:none;color:white;cursor:pointer;font-size:14px; padding:0; margin-left:4px;">🔍</button>
+    <button id="fly-btn" style="background:none;border:none;color:white;cursor:pointer;font-size:12px; padding:0; margin-left:4px;">🔍</button>
   </div>
-        <div style="font-size:10px;font-weight:bold; display:flex; justify-content:space-between; align-items:center; background:none; border:none; color:white; cursor:pointer;">📍 ${coords[0].toFixed(2)}, ${coords[1].toFixed(2)}, ${flag} </div>
+        <div style="font-size:12px;font-weight:bold; display:flex; justify-content:space-between; align-items:center; background:none; border:none; color:white; cursor:pointer;">📍 ${coords[0].toFixed(2)}, ${coords[1].toFixed(2)}, ${flag} </div>
       `;
       panel.style.display = 'block';
       lastMarker = marker;
