a0d4d091d7cc59ede8b7c919f80809b0c86854f2
Archeoisio <m.musio@yahoo.it>
2025-09-19 23:55:29 +0200

    Update map-flag.js

--- map-flag.js
+++ map-flag.js
@@ -335,14 +335,14 @@ controlBox.onAdd = function(map) {
     startInput.id = 'start';
     startInput.placeholder = 'Partenza';
     startInput.style.marginBottom = '4px';
-    startInput.style.width = '95%';
+    startInput.style.width = '100%';
     routeBox.appendChild(startInput);
 
     const endInput = document.createElement('input');
     endInput.id = 'end';
     endInput.placeholder = 'Destinazione';
     endInput.style.marginBottom = '4px';
-    endInput.style.width = '95%';
+    endInput.style.width = '100%';
     routeBox.appendChild(endInput);
 
   const buttonRow = document.createElement('div');
