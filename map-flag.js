961303c13758fcd719d65d765240254067975c8c
Archeoisio <m.musio@yahoo.it>
2026-01-25 12:01:48 +0100

    Update map-flag.js

--- map-flag.js
+++ map-flag.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', () => {
     zoom: initialView.zoom,
     layers: [satellite],
     zoomControl: true,
-    minZoom: isMobile ? 2 : 2.5,
+    minZoom: isMobile ? 2.5 : 2.5,
     maxZoom: 18,
     worldCopyJump: true,
     maxBounds: maxBounds,
