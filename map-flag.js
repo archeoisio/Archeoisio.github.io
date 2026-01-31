06b7d3082a508336e699e2be046400e4ddf1a1e4
Archeoisio <m.musio@yahoo.it>
2026-01-31 19:17:27 +0100

    Update map-flag.js

--- map-flag.js
+++ map-flag.js
@@ -336,7 +336,7 @@ capitalsData.forEach(cap => {
     marker.bindTooltip(cap.name, {
         permanent: true,       // Sempre visibile
         direction: 'right',    // Forza la posizione a DESTRA
-        offset: [1, 0],        // Sposta l'etichetta di 5px a destra del punto
+        offset: [0, 0],        // Sposta l'etichetta di 5px a destra del punto
         className: 'capital-label' // Classe CSS per lo stile
     });
 
