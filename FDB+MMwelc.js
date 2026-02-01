dba1896ea8b54d210c1cdf99814e3c4e2c8401a0
Archeoisio <m.musio@yahoo.it>
2026-02-01 13:26:50 +0100

    Update FDB+MMwelc.js

--- FDB+MMwelc.js
+++ FDB+MMwelc.js
@@ -892,7 +892,7 @@ function showStep() {
         const rect = target.getBoundingClientRect();
         if (isHeartBox) {
             // Posizione sopra il box cuore
-            popupPoint = map.containerPointToLayerPoint([rect.left + (rect.width / 2), rect.top]);
+            popupPoint = map.containerPointToLayerPoint([rect.left + 50, rect.top]);
         } else {
             // Posizione standard a sinistra dei pulsanti
             popupPoint = map.containerPointToLayerPoint([rect.left - 170, rect.top + 85]);
