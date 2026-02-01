cbdec991595567718758973cf8081bd6a73db2b7
Archeoisio <m.musio@yahoo.it>
2026-02-01 18:18:08 +0100

    Update FDB+MMwelc.js

--- FDB+MMwelc.js
+++ FDB+MMwelc.js
@@ -898,7 +898,7 @@ function showStep() {
             popupPoint = map.containerPointToLayerPoint([rect.left + 100, rect.top -10]);
         } else {
             // Posizione standard a sinistra dei pulsanti
-            popupPoint = map.containerPointToLayerPoint([rect.left - 200, rect.top + 100]);
+            popupPoint = map.containerPointToLayerPoint([rect.left - 100, rect.top + 105]);
         }
     }
 
